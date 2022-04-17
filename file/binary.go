cbd7e98501850c651b32d7d079ec021538afeab7
ysicing <i@ysicing.me>
2022-04-17 19:52:04 +0800

    feat(file): add check file is file or binary
    add check file is file or binary
    
    Signed-off-by: ysicing <i@ysicing.me>

---
+++ file/binary.go
@@ -0,0 +1,40 @@
+package file
+
+import (
+	"bufio"
+	"os"
+)
+
+/* https://github.com/zgg2001/StringFinderZ/blob/master/src/detect/detect_binary.go
+https://blog.csdn.net/qq_45698148/article/details/120930607
+*/
+
+// IsBinary returns true if the file is binary.
+func IsBinary(path string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	r := bufio.NewReader(file)
+	buf := make([]byte, 1024)
+	n, err := r.Read(buf)
+
+	white_byte := 0
+	for i := 0; i < n; i++ {
+		if (buf[i] >= 0x20 && buf[i] <= 0xff) ||
+			buf[i] == 9 ||
+			buf[i] == 10 ||
+			buf[i] == 13 {
+			white_byte++
+		} else if buf[i] <= 6 || (buf[i] >= 14 && buf[i] <= 31) {
+			return true
+		}
+	}
+
+	if white_byte >= 1 {
+		return false
+	}
+	return true
+}
