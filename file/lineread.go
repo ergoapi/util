e84480e76ed2e769f538ac333426958217d96dba
ysicing <i@ysicing.me>
2023-05-08 16:08:02 +0800

    feat(update): update
    update
    
    Signed-off-by: ysicing <i@ysicing.me>

--- file/lineread.go
+++ file/lineread.go
@@ -1,7 +1,6 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-// Package file lineread reads lines from files. It's not fancy, but it got repetitive.
 package file
 
 import (
