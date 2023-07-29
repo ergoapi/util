d70bfbcb9748de29335c09f524e75df5ff3d4263
ysicing <i@ysicing.me>
2023-07-29 10:32:31 +0800

    feat(common): update
    update
    
    Signed-off-by: ysicing <i@ysicing.me>

---
+++ common/default.go
@@ -0,0 +1,16 @@
+package common
+
+const (
+	Default114Dns    = "114.114.114.114"
+	DefaultGoogleDns = "8.8.8.8"
+	DefaultCFDns     = "1.1.1.1"
+
+	DefaultFileMode0777 = 0o777
+	DefaultFileMode0755 = 0o755
+	DefaultFileMode0644 = 0o644
+	DefaultFileMode0600 = 0o600
+
+	DefaultGoogleGenerate204URL = "https://connectivitycheck.gstatic.com/generate_204"
+	DefaultMiuiGenerate204URL   = "https://connect.rom.miui.com/generate_204"
+	DefaultV2exGenerate204URL   = "https://captive.v2ex.co/generate_204"
+)
