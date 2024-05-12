ccf9b57e4cb87aac87ca8c182e015070d557ae4e
ysicing <i@ysicing.me>
2024-05-12 20:48:19 +0800

    feat: refactor exgin package and add new routes
    - Refactored the main function in `examples/exgin/exgin.go` to use the `github.com/ergoapi/util/exgin` package
    - Added two new routes `/ping` and `/error` in the `examples/exgin/exgin.go` file
    - Modified the `customRespDone` function in `exgin/resp.go` to accept a `traceId` parameter
    - Modified the `renderMessage` function in `exgin/resp.go` to accept a `traceId` parameter
    - Modified the `GinsData` function in `exgin/resp.go` to use the `traceId` header value
    - Modified the `GinsCodeData` function in `exgin/resp.go` to use the `traceId` header value
    - Modified the `GinsErrorData` function in `exgin/resp.go` to use the `traceId` header value
    - Modified the `GinsAbort` function in `exgin/resp.go` to use the `traceId` header value
    - Modified the `GinsAbortWithCode` function in `exgin/resp.go` to use the `traceId` header value
    - Added a new function `BindWithErr` in `exgin/resp.go` to bind JSON with error handling
    
    Signed-off-by: ysicing <i@ysicing.me>

--- examples/exgin/exgin.go
+++ examples/exgin/exgin.go
@@ -1,11 +1,25 @@
 package main
 
-import "github.com/ergoapi/util/exgin"
+import (
+	"fmt"
+
+	"github.com/ergoapi/util/exgin"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	g := exgin.Init(&exgin.Config{
 		Debug: true,
 	})
 	g.Use(exgin.ExHackHeader())
+	g.GET("/ping", func(ctx *gin.Context) {
+		exgin.GinsData(ctx, "pong", nil)
+	})
+	g.GET("/error", func(ctx *gin.Context) {
+		exgin.GinsData(ctx, "pong", fmt.Errorf("error"))
+	})
+	g.GET("/errormap", func(ctx *gin.Context) {
+		exgin.GinsCodeData(ctx, 351, "pong", fmt.Errorf("451error"))
+	})
 	g.Run()
 }
--- exgin/resp.go
+++ exgin/resp.go
@@ -9,75 +9,77 @@ import (
 )
 
 // customRespDone done
-func customRespDone(code int, message, tid, data interface{}) gin.H {
+func customRespDone(code int, traceId string, data, message interface{}) gin.H {
+	msg := message
+	switch t := message.(type) {
+	case error:
+		msg = t.Error()
+	}
 	return gin.H{
 		"data":      data,
-		"message":   message,
+		"message":   msg,
 		"timestamp": ztime.NowUnix(),
 		"code":      code,
-		"traceId":   tid,
+		"traceId":   traceId,
 		"success":   code == 200,
 	}
 }
 
 // respDone done
-func respDone(code int, tid, data interface{}) gin.H {
-	return customRespDone(code, "请求成功", tid, data)
+func respDone(code int, traceId string, data interface{}) gin.H {
+	return customRespDone(code, traceId, data, "请求成功")
 }
 
 // respError error
-func respError(code int, message, tid interface{}) gin.H {
-	return customRespDone(code, message, tid, nil)
+func respError(code int, traceId string, message interface{}) gin.H {
+	return customRespDone(code, traceId, nil, message)
 }
 
-func renderMessage(c *gin.Context, v interface{}) {
-	tid := c.Writer.Header().Get("X-Trace-Id")
+func renderMessage(c *gin.Context, traceId string, v interface{}) {
 	if v == nil {
-		c.JSON(200, respDone(200, tid, nil))
+		c.JSON(200, respDone(200, traceId, nil))
 		return
 	}
 
 	switch t := v.(type) {
 	case string:
-		c.JSON(200, respError(10400, tid, t))
+		c.JSON(200, respError(10400, traceId, t))
 	case error:
-		c.JSON(200, respError(10400, tid, t.Error()))
+		c.JSON(200, respError(10400, traceId, t.Error()))
 	}
 }
 
 func GinsData(c *gin.Context, data interface{}, err error) {
-	tid := c.Writer.Header().Get("X-Trace-Id")
+	traceId := c.Writer.Header().Get("X-Trace-Id")
 	if err == nil {
-		c.JSON(200, respDone(200, tid, data))
+		c.JSON(200, respDone(200, traceId, data))
 		return
 	}
-
-	renderMessage(c, err.Error())
+	renderMessage(c, traceId, err.Error())
 }
 
 func GinsCodeData(c *gin.Context, code int, data interface{}, err error) {
-	tid := c.Writer.Header().Get("X-Trace-Id")
+	traceId := c.Writer.Header().Get("X-Trace-Id")
 	if err == nil {
-		c.JSON(200, respDone(code, tid, data))
+		c.JSON(200, respDone(code, traceId, data))
 		return
 	}
-
-	renderMessage(c, err.Error())
+	renderMessage(c, traceId, err.Error())
 }
 
 func GinsErrorData(c *gin.Context, code int, data interface{}, err error) {
-	tid := c.Writer.Header().Get("X-Trace-Id")
-	c.JSON(200, customRespDone(code, fmt.Sprintf("%v", err), tid, data))
+	traceId := c.Writer.Header().Get("X-Trace-Id")
+	c.JSON(200, customRespDone(code, traceId, data, err))
 }
 
 func GinsAbort(c *gin.Context, msg string, args ...interface{}) {
-	tid := c.Writer.Header().Get("X-Trace-Id")
-	c.AbortWithStatusJSON(200, respError(10400, fmt.Sprintf(msg, args...), tid))
+	traceId := c.Writer.Header().Get("X-Trace-Id")
+	c.AbortWithStatusJSON(200, respError(10400, traceId, fmt.Sprintf(msg, args...)))
 }
 
 func GinsAbortWithCode(c *gin.Context, respcode int, msg string, args ...interface{}) {
-	tid := c.Writer.Header().Get("X-Trace-Id")
-	c.AbortWithStatusJSON(200, respError(respcode, fmt.Sprintf(msg, args...), tid))
+	traceId := c.Writer.Header().Get("X-Trace-Id")
+	c.AbortWithStatusJSON(200, respError(respcode, traceId, fmt.Sprintf(msg, args...)))
 }
 
 func GinsCustomResp(c *gin.Context, obj interface{}) {
@@ -93,6 +95,15 @@ func Bind(c *gin.Context, ptr interface{}) {
 	if err != nil {
 		errors.Bomb("参数不合法: %v", err)
 	}
+}
+
+// BindWithErr bind with error
+func BindWithErr(c *gin.Context, ptr interface{}) error {
+	err := c.ShouldBindJSON(ptr)
+	if err != nil {
+		return fmt.Errorf("参数不合法: %v", err)
+	}
+	return nil
 }
 
 // APICustomRespBody swag api resp body
